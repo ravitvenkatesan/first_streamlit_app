5b1a4bf938ba2f910467cdbb29817053f99baada
Ravi Venkatesan <ravitvenkatesan@users.noreply.github.com>
2023-06-26 11:51:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,12 @@ print('Omega 3 and Blueberry Oatmeal')
 print('Kale, Spinach and Rocket Smoothie')
 print('Hard-boiled Free-Range Egg')
 
-```
-<html>
-<head><title>Breakfast Menu</title></head>
-<body>
-  <p>Omega 3 and Blueberry Oatmeal</p>
-  <p>Kale, Spinach and Rocket Smoothie</p>
-  <p>Hard-boiled Free-Range Egg</p>
-</body>
-</html>
-```
+#<html>
+#<head><title>Breakfast Menu</title></head>
+#<body>
+  #<p>Omega 3 and Blueberry Oatmeal</p>
+  #<p>Kale, Spinach and Rocket Smoothie</p>
+  #<p>Hard-boiled Free-Range Egg</p>
+#</body>
+#</html>
+
