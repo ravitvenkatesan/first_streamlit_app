17dd3abe5d9d158718c3fd5860c3be06bbc88ed3
Ravi Venkatesan <ravitvenkatesan@users.noreply.github.com>
2023-06-26 11:36:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 # created the main python file
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-print('<html>
+print('Breakfast Menu')
+print('Omega 3 and Blueberry Oatmeal')
+print('Kale, Spinach and Rocket Smoothie')
+print('Hard-boiled Free-Range Egg')
+/*<html>
 <head><title>Breakfast Menu</title></head>
 <body>
   <p>Omega 3 and Blueberry Oatmeal</p>
   <p>Kale, Spinach and Rocket Smoothie</p>
   <p>Hard-boiled Free-Range Egg</p>
 </body>
-</html>
-')
+</html>*/
