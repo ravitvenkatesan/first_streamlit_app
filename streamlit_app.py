b59dc464a275dc28f3d5d7e41d0ff7b4ea2e6c60
Ravi Venkatesan <ravitvenkatesan@users.noreply.github.com>
2023-06-27 11:07:35 -0500

    Update streamlit_app.py
    Changed the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # created the main python file
 import streamlit
 import pandas
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
