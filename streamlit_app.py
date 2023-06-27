661decb5f9069cd0dc38ed4e7c064d352961b2d3
Ravi Venkatesan <ravitvenkatesan@users.noreply.github.com>
2023-06-27 11:06:12 -0500

    Update streamlit_app.py
    Added column Fruit as the list index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-#streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #Put a picklist so they can pick the fruits they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #Display the table on the page
